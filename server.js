407e5b908b4ee132f87bb91e2f0f72df0c9052e1
David Hausken <david-hausken@uiowa.edu>
2019-05-07 19:27:07 -0500

    MongoDB printing findOne

--- server.js
+++ server.js
@@ -2,31 +2,39 @@
 
 
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
-let db;
-const url = 'mongodb://localhost:27017/project';
+const url = 'mongodb://localhost:27017/';
 // start the express web server listening on 8080
-MongoClient.connect(url, (err, database) => {
+app.listen(8080, () => {
+    console.log('listening on 8080');
+});
+
+MongoClient.connect(url, (err, db) => {
     if(err) {
         return console.log(err)
     }
-    db = database;
-    app.listen(8080, () => {
-        console.log('listening on 8080');
-    });
-});
+    else
+    {
+        console.log("Successful connection")
+    }
 
+    var dbo = db.db("project");
+    app.get('/clicked',function (req, res) {
+        console.log("Res Output:");
+        console.log(JSON.stringify(req.query.STATE));
+        dbo.collection("deaths").findOne({}, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    })
+});
 
 // serve the homepage
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/clicked',function (req, res) {
-    console.log("Res Output:");
-    console.log(res.param('STATE'));
-});
